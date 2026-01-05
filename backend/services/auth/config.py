05b6a2f2fc8a07780bdcb93784955eccc2987044
Nana Adeku <nans.@Nanas-MacBook-Pro.local>
2026-01-05 20:35:27 +1100

    Add comment

--- backend/services/auth/config.py
+++ backend/services/auth/config.py
@@ -12,8 +12,9 @@ class AuthSettings:
 
 
 def get_auth_settings() -> AuthSettings:
+    # These defaults are set to ensure that it's easy to run. This would not be done in production
     return AuthSettings(
-        jwt_secret_key=os.getenv("JWT_SECRET_KEY", ""),
+        jwt_secret_key=os.getenv("JWT_SECRET_KEY", "test"),
         jwt_algorithm=os.getenv("JWT_ALGORITHM", "HS256"),
         access_token_exp_minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", "60")),
         default_email=os.getenv("DEFAULT_AUTH_EMAIL", "test@gmail.com"),
